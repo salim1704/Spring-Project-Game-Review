cbff9cda1bf6f7c35763a3a576622a50e79b5ef2
abdul salim <qhs9876@gmail.com>
2022-04-26 23:41:44 +0100

    game service created

--- src/main/java/com/qa/GameReview/repository/ReviewRepository.java
+++ src/main/java/com/qa/GameReview/repository/ReviewRepository.java
@@ -1,5 +1,7 @@
 package com.qa.GameReview.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.qa.GameReview.entity.Review;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Integer>{
+	List<Review> findByGameId(int id);
 
 }
--- src/main/java/com/qa/GameReview/service/GameService.java
+++ src/main/java/com/qa/GameReview/service/GameService.java
@@ -1,19 +1,20 @@
 package com.qa.GameReview.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.qa.GameReview.dto.GameDTO;
+import com.qa.GameReview.dto.NewGameDTO;
+import com.qa.GameReview.dto.ReviewDTO;
 import com.qa.GameReview.entity.Game;
-import com.qa.GameReview.entity.Review;
 import com.qa.GameReview.repository.GameRepository;
 
 @Service
@@ -22,7 +23,7 @@ public class GameService {
 	private GameRepository gameRepository;
 	private ReviewService reviewService;
 	private ModelMapper modelMapper;
-	
+
 	@Autowired
 	public GameService(GameRepository gameRepository, ReviewService reviewService, ModelMapper modelMapper) {
 		super();
@@ -31,29 +32,62 @@ public class GameService {
 		this.modelMapper = modelMapper;
 	}
 	
-	private GameDTO mapToDTO(Game game) {
-        return modelMapper.map(game, GameDTO.class);
-    }
-	public List<Game> readAll() {
-		return gameRepository.findAll();
+	public GameDTO mapToDTO(Game game) {
+		return this.modelMapper.map(game, GameDTO.class);
+	}
+	
+	public List<GameDTO> getGames() {
+		List<Game> games = gameRepository.findAll();
+		List<GameDTO> DTOS = new ArrayList<>();
+		
+		for (Game game : games) {
+			DTOS.add(this.mapToDTO(game));
+		}
+		return DTOS;
 	}
 
-	public Game readById(int id) {
+	public GameDTO getGame(int id) {
 		Optional<Game> game = gameRepository.findById(id);
 
 		if (game.isPresent()) {
-			return game.get();
+			return this.mapToDTO(game.get());
 		}
-		throw new EntityNotFoundException("Game with id " + id + " was not found");
+		throw new EntityNotFoundException("Game with id " + id + " not found");
+//		return this.toDTO(user.orElseThrow(() -> new EntityNotFoundException("User not found with id " + id)));
 	}
 
-	public List<Review> readReviewByGameId(int id) {
-		Game game = this.readById(id);
-		return game.getReviews();
+	public GameDTO createGame(NewGameDTO game) {
+		Game toSave = this.modelMapper.map(game, Game.class);
+		Game newGame = gameRepository.save(toSave);
+		return this.mapToDTO(newGame);
 	}
 
-	public Game create(Game game) {
-		return gameRepository.save(game);
+	@Transactional
+	public GameDTO updateGame(NewGameDTO game, int id) {
+		if (gameRepository.existsById(id)) {
+			Game savedGame = gameRepository.getById(id);
+			savedGame.setTitle(game.getTitle());
+			savedGame.setGenre(game.getGenre());
+			savedGame.setAgeRating(game.getAgeRating());
+			savedGame.setReleaseYear(game.getReleaseYear());
+			gameRepository.save(savedGame);
+			return this.mapToDTO(gameRepository.save(savedGame));
+		}
+		throw new EntityNotFoundException("Game with id " + id + " not found");
 	}
 
+	public void deleteGame(int id) {
+		if (gameRepository.existsById(id)) {
+			gameRepository.deleteById(id);
+			return;
+		}
+		throw new EntityNotFoundException("Game with id " + id + " not found");
+	}
+	
+	public List<ReviewDTO> getGameReviews(int gameId) {
+		GameDTO game = this.getGame(gameId);
+		List<ReviewDTO> reviews = reviewService.getReviewsByGameId(gameId);
+		reviews.forEach(review -> review.setGameDTO(game));
+		return reviews;
+	}
 }
