b94abb89660549a6cf495f90f1ddf8ec80422767
abdul salim <qhs9876@gmail.com>
2022-04-29 01:00:18 +0100

    minor change in review controller

--- src/main/java/com/qa/GameReview/controller/ReviewController.java
+++ src/main/java/com/qa/GameReview/controller/ReviewController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.qa.GameReview.service.ReviewService;
 
 @RestController
 @RequestMapping(path = "/review")
+@CrossOrigin("*")
 public class ReviewController {
 
 	private ReviewService reviewService;
